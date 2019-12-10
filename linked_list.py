529540899ba06d145e07c1573b36fea9a0370a9d
Md. Tanvir Ahmed <tanvirahmed.cse.bubt@gmail.com>
2019-12-10 18:43:04 +0600

    resolved errors

--- linked_list.py
+++ linked_list.py
@@ -1,5 +1,5 @@
 class node:
-	def __init__(self, data=None, next_node=Node):
+	def __init__(self, data=None, next_node=None):
 		self.data = data
 		self.next_node = next_node
 
@@ -17,16 +17,27 @@ class node:
 
 class linkedlist:
 	def __init__(self):
-		start = node()
+		self.start = node()
 
 	def insert(self, data):
-		newnd = node(data, start.next_node)
+		newnd = node(data, self.start.next_node)
 		self.start.set_next(newnd)
 
-	def traverse(self, data):
-		current = start
+	def traverse(self):
+		current = self.start
 		while current.next_node != None:
 			current = current.next_node
-			print(current.data)
+			print(current.data, end=" ")
+
+		print("\n")
 			
+if __name__ == "__main__":
+	lnkedlist = linkedlist()
+	lnkedlist.insert(5)
+	lnkedlist.insert(7)
+	lnkedlist.insert(1)
+	lnkedlist.insert(9)
+	lnkedlist.insert(12)
+	lnkedlist.insert(14)
+	lnkedlist.traverse()
 
