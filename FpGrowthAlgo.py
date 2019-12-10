22536672da9119256055be0d357ad9a8ca99e9fd
Md. Tanvir Ahmed <tanvirahmed.cse.bubt@gmail.com>
2019-12-10 22:11:07 +0600

    Added FpGrowthAlgo.py

---
+++ FpGrowthAlgo.py
@@ -0,0 +1,93 @@
+class node:
+	def __init__(self, data=None, next_node=None):
+		self.data = data
+		self.next_node = next_node
+
+	def get_data(self):
+		return self.data
+
+	def get_next(self):
+		return self.next_node
+
+	def set_data(self, data):
+		self.data = data
+
+	def set_next(self, nxt):
+		self.next_node = nxt
+
+
+class Linkedlist:
+	def __init__(self):
+		self.start = node()
+
+	def insert(self, data):
+		newnd = node(data, self.start.next_node)
+		self.start.set_next(newnd)
+
+	def traverse(self):
+		current = self.start
+		while current.next_node != None:
+			current = current.next_node
+			print(current.data, end=" ")
+
+		print("\n")
+
+
+class FpNode:
+    def __init__(self):
+        self.child = []
+        self.item = {}
+
+
+import operator
+class FpGrowthTree:
+    def __init__(self, transactions, L1):
+        self.Null = FpNode()
+        self.transactions = []
+        self.L1 = dict( sorted(L1.items(), key=operator.itemgetter(1), reverse=True))
+        print(self.L1)
+        self.links = {}
+        for x in self.L1:
+            self.links[x] = Linkedlist()
+        
+        indx = 0
+        for i in transactions:
+            tran = []
+            for x in self.L1:
+                if x in transactions[indx]:
+                    tran.append(x)
+            
+            self.transactions.append(tran)
+            indx = indx + 1
+    
+    def generate_tree(self, itm):
+        flg = True
+        for chld in self.Null.child:
+            if itm in chld.item:
+                chld.item[itm] = chld.item[itm] + 1
+            else:
+                flg = False
+
+
+transactions = []
+transactions.append(['I1', 'I2', 'I5'])
+transactions.append(['I2', 'I4'])
+transactions.append(['I2','I3'])
+transactions.append(['I1', 'I2', 'I4'])
+transactions.append(['I1', 'I3'])
+transactions.append(['I2', 'I3'])
+transactions.append(['I1', 'I3'])
+transactions.append(['I1', 'I2', 'I3', 'I5'])
+transactions.append(['I1', 'I2', 'I3'])
+
+L1 = {}
+L1['I1'] = 6
+L1['I2'] = 7
+L1['I3'] = 6
+L1['I4'] = 2
+L1['I5'] = 2
+
+print(transactions)
+fp_growth_tree = FpGrowthTree(transactions, L1)
+print(fp_growth_tree.transactions)
+
