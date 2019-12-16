232f753b674eef57b3985972c3d418bfd6b86d3c
Md. Tanvir Ahmed <tanvirahmed.cse.bubt@gmail.com>
2019-12-17 02:47:11 +0600

    added analysis table, added conditional_pattern_base, removed path method

--- FpGrowthAlgo.py
+++ FpGrowthAlgo.py
@@ -11,8 +11,10 @@ class FpGrowthTree:
 		self.Null = FpNode()
 		self.transactions = []
 		self.L1 = dict( sorted(L1.items(), key=operator.itemgetter(1), reverse=True)) #sorting desc order L1 dictionary based on values
+		self.l1 = dict( sorted(L1.items(), key=operator.itemgetter(1))) #sorting asc order L1 dictionary based on values
 		#print(self.L1)
 		self.links = {}
+		self.analysis = {} # dictionary which key'll be a item and value'll be a list of 3 lists. one list is for conditional pattern base, one for conditional fp tree and one for FP generation
 
 		for x in self.L1:
 			self.links[x] = []
@@ -55,18 +57,40 @@ class FpGrowthTree:
 		for chld in node.child:
 			self.traverse_tree(chld)
 
-	def path(self, itm):
-		i = 0
+	def genetate_conditional_pattern_base(self, itm):
+		cp_base = {}
 		for ocrns in self.links[itm]:
-			i = i + 1
-			print("Path of occurrence: {}".format(i))
+			sup = ocrns.item[itm]
+			itmst = set()
 			current = ocrns
 			while current.parent.parent != None:
-				print(current.parent.item)
+				#print(current.parent.item)
+				itmst.add(list(current.parent.item.keys())[0])
+
 				current = current.parent
+			cp_base[frozenset(itmst)] = sup
 
-			print()
+		return cp_base
 
+
+	def conditional_pattern_base(self):
+		#print(self.analysis)
+		for x in self.analysis:
+			self.analysis[x].append(self.genetate_conditional_pattern_base(x))
+
+
+	def do_analysis(self):
+		for x in self.l1:
+			null_attached = True
+			for ocrns in self.links[x]:
+				if ocrns.parent.parent != None:
+					null_attached = False
+					break
+
+			if null_attached == False:
+				self.analysis[x]=[]
+
+		self.conditional_pattern_base()
 
 
 
@@ -94,6 +118,8 @@ fp_growth_tree = FpGrowthTree(transactions, L1)
 fp_growth_tree.generate_tree()
 print("\nDFS travarsal of Fp Growth Tree:")
 fp_growth_tree.traverse_tree(fp_growth_tree.Null)
-fp_growth_tree.path('I3')
+#fp_growth_tree.path('I3')
+fp_growth_tree.do_analysis()
+print(fp_growth_tree.analysis)
 
 
