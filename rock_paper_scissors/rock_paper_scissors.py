b8ce931048f67ebec3c7e0b620af65d5a1f14727
Md. Tanvir Ahmed <tanvir66288@gmail.com>
2025-10-27 13:11:38 +0600

    TA | Fix punctuation in game description

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -4,7 +4,7 @@ Rock paper scissors is a hand game usually played between two people, in which e
 It has only two possible outcomes: a draw, or a win for one player and a loss for the other.
 A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors") but will lose to one who has played paper ("paper covers rock")
 A play of paper will lose to a play of scissors ("scissors cuts paper")
-If both players choose the same shape, the game is tied
+If both players choose the same shape, the game is tied.
 
 """
 
